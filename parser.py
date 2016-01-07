274c44b7f1ae86a8f755154a18e9547937943519
Stephen Roberts <stephenianroberts@gmail.com>
2016-01-07 11:54:13 +0000

    added internal-only message logic

--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ import argparse
 import codecs
 import json
 import os
+import re
 import operator
 import email.parser
 import dateutil.parser
@@ -21,6 +22,8 @@ def process_arguments():
     parser.add_argument("-o", "--output", help='Path to output file',
                         required=True)
     parser.add_argument("-u", "--unique", help="Remove Duplicate messages",
+                        action="store_true")
+    parser.add_argument("-i", "--internal", help="Remove external messages",
                         action="store_true")
     parser.add_argument("-v", "--verbose", help="increase output verbosity",
                         action="store_true")
@@ -41,11 +44,20 @@ def read_message(path):
                        message['Subject'],
                        message.get_payload())
 
-def load_messages(path, unique, verbose):
+def is_internal(message):
+    """ Checks to see if a message is internal to Enron """
+    enron_re = re.compile('@enron.com')
+    if not enron_re.search(message.sender):
+        return False
+    if len(message.recipients) == 0:
+        return False
+    return all(r for r in message.recipients if enron_re.search(r))
+
+def load_messages(path, unique, internal, verbose):
     """ Loads messages from the corpus and returns them as Message objects """
     if unique:
-        messages = set()
-        ingest_method = messages.add
+        uniques = set()
+        ingest_method = uniques.add
     else:
         messages = list()
         ingest_method = messages.append
@@ -54,10 +66,13 @@ def load_messages(path, unique, verbose):
             print("Processing {}".format(root))
         for message_file in files:
             message = read_message(os.path.join(root, message_file))
+            if internal and not is_internal(message):
+                continue
             ingest_method(message)
     if unique:
-        messages = list(messages)
-    return messages
+        return list(uniques)
+    else:
+        return messages
 
 def output_messages(path, messages):
     """ Serializes messages to JSON """
@@ -70,7 +85,7 @@ def output_messages(path, messages):
 def main():
     """ Applicaion entry point """
     args = process_arguments()
-    messages = load_messages(args.path, args.unique, args.verbose)
+    messages = load_messages(args.path, args.unique, args.internal, args.verbose)
     messages.sort(key=operator.attrgetter('timestamp'))
     output_messages(args.output, messages)
 
