f7032e16b217620e45e0c822a58c99e3e77d9805
Stephen Roberts <stephenianroberts@gmail.com>
2016-01-12 15:30:24 +0000

    changed help message capitalization

--- graph_parse.py
+++ graph_parse.py
@@ -16,23 +16,25 @@ Message = namedtuple('Message', ['sender', 'recipients', 'timestamp'])
 def process_arguments():
     """ Process command line arguments """
     parser = argparse.ArgumentParser(description="Enron Corpus Parser")
-    parser.add_argument("-a", "--aliases", help='Path to Alias file',
+    parser.add_argument("-a", "--aliases", help='path to Alias file',
                         type=argparse.FileType('r'), required=True)
-    parser.add_argument("-o", "--output", help='Path to output file',
+    parser.add_argument("-o", "--output", help='path to output file',
                         type=argparse.FileType('w'), required=True)
-    parser.add_argument("-p", "--path", help='Path to Enron corpus',
+    parser.add_argument("-p", "--path", help='path to Enron corpus',
                         required=True)
+    parser.add_argument("-s", "--sent", help="only parse sent message folders",
+                        action="store_true")
     parser.add_argument("-v", "--verbose", help="increase output verbosity",
                         action="store_true")
     return parser.parse_args()
 
-def message_extractor(path, address_book, verbose):
+def message_extractor(path, address_book, outboxes, verbose):
     """ Extracts e-mails from the Enron corpus """
     parser = email.parser.Parser()
     outbox_re = [re.compile(r) for r in ['sent_items$', 'sent$', 'sent_mail$']]
     for root, _, files in os.walk(path):
         # Only parse messages in 'sent' folder
-        if not any(re.search(root) for re in outbox_re):
+        if outboxes and not any(re.search(root) for re in outbox_re):
             continue
         if verbose:
             print(root)
@@ -68,7 +70,7 @@ def main():
                                 for email in emails}
     args.aliases.close()
     # Set ensures messages are unique
-    messages = set(message_extractor(args.path, address_book, args.verbose))
+    messages = set(message_extractor(args.path, address_book, args.sent, args.verbose))
     save_messages(messages, args.output)
 
 if __name__ == '__main__':
